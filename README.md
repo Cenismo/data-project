9be59466f4485e6658475b08f9c195ff37e93e94
Pablo <pablors.microcamp@gmail.com>
2024-10-29 10:43:34 -0300

    feat: adicionar feature de extract e load

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # data-project
-<<<<<<< HEAD
-=======
-
->>>>>>> b46713775ecb0dbb5c307619e97cf31c2f61d757
+Testando
---
+++ src/TESTE.py
@@ -0,0 +1,7 @@
+import os
+
+print("DB_HOST_PROD:", os.getenv('DB_HOST_PROD'))
+print("DB_PORT_PROD:", os.getenv('DB_PORT_PROD'))
+print("DB_NAME_PROD:", os.getenv('DB_NAME_PROD'))
+print("DB_USER_PROD:", os.getenv('DB_USER_PROD'))
+print("DB_PASS_PROD:", os.getenv('DB_PASS_PROD'))
